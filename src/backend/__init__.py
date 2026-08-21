53a925d3d568aaf592e526740c38433c5a03d662
Daniil <lesnicenkodaniil62@gmail.com>
2026-08-21 17:13:38 +0300

    Update __init__.py

--- src/backend/__init__.py
+++ src/backend/__init__.py
@@ -1,1 +1,10 @@
-"""Здесь в файле (backend) находица backend часть кода"""
+"""
+Backend-пакет приложения.
+"""
+
+from typing import List
+
+from .app import RequestHandler, run_server
+
+__all__: List[str] = ["run_server", "RequestHandler"]
+__version__: str = "1.0.0"
