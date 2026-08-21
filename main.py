2bef525bdbb0e40735f18c69d4ef2284d2d0497c
Daniil <lesnicenkodaniil62@gmail.com>
2026-08-21 12:20:06 +0300

    feature_2 backend

--- main.py
+++ main.py
@@ -5,14 +5,13 @@
     python main.py
 """
 
-from typing import NoReturn
-
 from src.backend.app import run_server
 
 
-def main() -> NoReturn:
+def main() -> None:
     """Запускает веб-приложение."""
     run_server()
+    return None
 
 
 if __name__ == "__main__":
